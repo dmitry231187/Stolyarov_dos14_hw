d4488d373c3db8d7fb4e94eb7f5b10b917afa428
Dmitry Stolyarov <125599832+dmitry231187@users.noreply.github.com>
2023-03-28 21:39:45 +0300

    Update 822.sh

--- 822.sh
+++ 822.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # проверка на корректность url
+isUrl() {
 if [[ "$string" =~ ^(https?\:\/\/(www\.)?)?([[:alnum:]]+\.)+[[:alnum:]]+\/?$ ]]; then
  echo "$string - корректный url"
 else
  echo "это не url"
 fi
+}
