a516d75d318d21ae256af32f32d2601df3fb7d0a
Dmitry Stolyarov <125599832+dmitry231187@users.noreply.github.com>
2023-03-28 21:39:11 +0300

    Update 821.sh

--- 821.sh
+++ 821.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # проверка на корректность email
-if [[ "$string" =~ ^[[:alnum:]]+\@[[:alnum:]]+\.[[:alpha:]]+$ ]]; then
- echo "$string - корректный email"
-else
- echo "это не email"
-fi
+isEmail() {
+  if [[ "$string" =~ ^[[:alnum:]]+\@[[:alnum:]]+\.[[:alpha:]]+$ ]]; then
+  echo "$string - корректный email"
+ else
+  echo "это не email"
+ fi
+}
